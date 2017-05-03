c8171f03557dbd77d8639bce35acb36b4d7ed3ca
Wojciech Matusiak <saphir2357@gmail.com>
2017-05-03 14:28:57 +0200

    Implementacja PolyAddMonos, PolyMul oraz PolyClone; poprawki, żeby się budowało.

--- src/poly.c
+++ src/poly.c
@@ -3,6 +3,7 @@
  */
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 #include "poly.h"
 
 /**
@@ -49,7 +50,7 @@ static inline Poly PolyAddPC(const Poly *p, const Poly *q)
     int start_i = (p->monos[0].exp == 0) ? 0 : 1;
     result.monos = malloc(sizeof(Mono) * (p->length + start_i));
 
-    result.monos[0] = {.p = PolyFromCoeff(0), .exp = 0};
+    result.monos[0] = (Mono){.p = PolyFromCoeff(0), .exp = 0};
     for (int i = 0; i < p->length; ++i)
         result.monos[start_i] = MonoClone(p->monos + start_i + i);
 
@@ -85,7 +86,7 @@ static inline Poly PolyAddPP(const Poly *p, const Poly *q)
             result.monos[result_index] = MonoClone(q->monos + q_index);
             ++q_index;
         } else {
-            result.monos[result_index] = {
+            result.monos[result_index] = (Mono){
                 .p = PolyAdd(&p->monos[p_index].p, &q->monos[q_index].p),
                 .exp = p->monos[p_index].exp
             };
@@ -132,6 +133,145 @@ static inline poly_coeff_t QuickPower(poly_coeff_t base, poly_exp_t exponent)
     return QuickPowerTail(base, exponent, 1);
 }
 
+/**
+ * Mnoży wielomian przez \f$ x^n \f$.
+ * Mnoży/przesówa wielomian o zadany wykładnik: dodaje <c>shift</c> do wysztskich jego wykładników. Jeżeli <c>p</c> jest
+ * współczynnikiem, to zostanie przekształcony w wielomian o jednym monomianie.
+ * @param p wielomian, który zostanie przesunięty
+ * @param shift przesunięcie
+ */
+static void PolyShift(Poly *p, poly_exp_t shift)
+{
+    if (p->monos == NULL) {
+        p->monos = malloc(sizeof(Mono));
+        p->monos[0].p = PolyFromCoeff(p->asCoef);
+        p->monos->exp = shift;
+        p->length = 1;
+    } else {
+        for (poly_exp_t i = 0; i < p->length; ++i)
+            p->monos[i].exp += shift;
+    }
+}
+
+/**
+ * Mnoży wielomian przez skalar.
+ * @param p wielomian, który ma zostać pomnożony
+ * @param scalar skalar
+ */
+static void PolyScale(Poly *p, poly_coeff_t scalar)
+{
+    if (PolyIsCoeff(p)) {
+        p->asCoef *= scalar;
+    } else {
+        for (poly_exp_t i = 0; i < p->length; ++i)
+            PolyScale(&p->monos[i].p, scalar);
+    }
+}
+
+/**
+ * Zwraca wielomian pomnożony przez jednomian.
+ * @param p wielomian
+ * @param q jednomian
+ * @return \f$ q * q \f$
+ */
+static Poly PolyMulM(const Poly *p, const Mono *q)
+{
+    Poly result = PolyMul(p, &q->p);
+    PolyShift(&result, q->exp);
+    return result;
+}
+
+/**
+ * Scal dwie tablice posortowanych jednomianów do w tablicy wynikowej.
+ * Metoda zakłada, że <c>in1</c> jest na w swapowanej tablicy, a <c>in2</c> to sufiks tablicy <c>out</c>. Wynik scalenia
+ * umieszczony będzie w tablicy <c>out</c>, a tablica <c>in1</c> zostanie nietknięta. Jednomiany o tym samym wykładniku
+ * nie są scalane.
+ * @param in1 pierwsza z posortowanych tablic; rozłączna z <c>out</c>
+ * @param in2  druga z posortowanych tablic; sufiks tablicy <c>out</c>
+ * @param out tablica, w której zostanie zapisany wynik
+ * @param len1 długość tablicy <c>in1</c>
+ * @param len2 długość tablicy <c>in2</c>
+ */
+static void PolyMergeSortedMonos(Mono *in1, Mono *in2, Mono *out, int len1, int len2)
+{
+    poly_exp_t i = 0, j1 = 0, j2 = 0;
+    while (j1 < len1 && j2 < len2) {
+        Mono m;
+        if (in1[j1].exp < in2[j2].exp) {
+            m = in1[j1];
+            ++j1;
+        } else {
+            //Może być więcej niż dwa o równym stopniu, więc nie ma sensu teraz zajmować się równością stopni
+            m = in2[j2];
+            ++j2;
+        }
+        out[i++] = m;
+    }
+
+    if (j1 < len1)
+        memcpy(out + i, in1 + j1, (size_t)(len1 - j1));
+    //Zgodnie z założeniami, jeśli in2 się nie skończyło, to i tak jest już na swoim miejscu
+}
+
+/**
+ * Sortuje tablicę jednomianów niemalejąco według ich wykładnika.
+ * Jednomiany o tych samych wykładnikach nie są scalane.
+ * @param monos tablica jednomianów do posortowania
+ * @param length długość tablicy <c>monos</c>
+ */
+static void PolySortMonos(Mono *monos, poly_exp_t length)
+{
+    if (length == 1)
+        return;
+
+    poly_exp_t len1 = length / 2;
+    poly_exp_t len2 = length - len1;
+    Mono *sub1 = malloc(sizeof(Mono) * len1);
+    Mono *sub2 = monos + len1;
+
+    memcpy(sub1, monos, sizeof(Mono) * len1);
+    PolySortMonos(sub1, len1);
+    PolySortMonos(monos + len1, len2);
+
+    PolyMergeSortedMonos(sub1, sub2, monos, len1, len2);
+    free(sub1);
+}
+
+/**
+ * Tworzy wielomian zbudowany z sumy jednomianów podanych w tablicy.
+ * Wielomian przejmuje na własność tablicę jednomianów i staje się ona jego wewnętrzną tablicą. W związku z tym, tablica
+ * nowego wielomianu będzie za duża, kiedy w wejściowej tablicy <c>monos</c> będą jednomiany o takim samym wykładniku.
+ * Te nadmiarowe jednomiany będą znajdować się na końcu tablicy, ich wykładnik zostanie ustawiony na -1. Pole
+ * <c>length</c> struktury wielomianu będzie zawierało liczbę tylko prawidłowych jednomianów w tablicy, więc może być
+ * mniejsza niż wejściowa długość tablicy jednomianów.
+ * @param monos tablica jednomianów (przejmowana na własność)
+ * @param length długosć tablicy <c>monos</c>
+ * @return
+ */
+static Poly PolyFromMonos(Mono *monos, poly_exp_t length)
+{
+    PolySortMonos(monos, length);
+
+    int i = 0;
+    for (int j = 1; j < length; ++j) {
+        if (monos[i].exp == monos[j].exp) {
+            Poly p = PolyAdd(&monos[i].p, &monos[j].p);
+            PolyDestroy(&monos[i].p);
+            PolyDestroy(&monos[j].p);
+            monos[i].p = p;
+        } else {
+            ++i;
+            monos[i] = monos[j];
+            monos[j].exp = -1;
+        }
+    }
+
+    Poly p;
+    p.monos = monos;
+    p.length = i;
+    return p;
+}
+
 void PolyDestroy(Poly *p)
 {
     if (p->monos != NULL) {
@@ -155,4 +295,48 @@ Poly PolyAdd(const Poly *p, const Poly *q)
     if (p->monos == NULL)
         return PolyAddPC(q, p);
     return PolyAddPP(p, q);
+}
+
+Poly PolyMul(const Poly *p, const Poly *q)
+{
+    if (PolyIsCoeff(p))
+        PolyMul(q, p);
+    if (PolyIsCoeff(q)) {
+        Poly result = PolyClone(p);
+        PolyScale(&result, q->asCoef);
+        return result;
+    }
+
+    Poly fold = PolyZero();
+    for (poly_exp_t i = 0; i < q->length; ++i) {
+        Poly old_fold = fold;
+        Poly next = PolyMulM(p, q->monos + i);
+        fold = PolyAdd(&old_fold, &next);
+        PolyDestroy(&old_fold);
+    }
+
+    return fold;
+}
+
+Poly PolyAddMonos(unsigned count, const Mono *monos)
+{
+    Mono *m_copy = malloc(sizeof(Mono) * count);
+    memcpy(m_copy, monos, sizeof(Mono) * count);
+    ///FIXME skoro przejmuje na własność to hyba nie może być const? Powinienem jeszcze pamięć zwolnić?
+    return PolyFromMonos(m_copy, count);
+}
+
+Poly PolyClone(const Poly *p)
+{
+    Poly result;
+    if (PolyIsCoeff(p)) {
+        result.monos = NULL;
+        result.asCoef = p->asCoef;
+    } else {
+        result.length = p->length;
+        result.monos = malloc(sizeof(Mono) * result.length);
+        for (int i = 0; i < result.length; ++i)
+            result.monos[i] = MonoClone(&p->monos[i]);
+    }
+    return result;
 }
