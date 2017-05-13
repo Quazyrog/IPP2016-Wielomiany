3e3f8e00c1690420c939e866a586a9009629a70c
Wojciech Matusiak <saphir2357@gmail.com>
2017-05-13 10:16:12 +0200

    Przeniesienie testów do osobnego pliku, żeby zrobić miejsce na oficjalne.

---
+++ src/test_poly_core.h
@@ -0,0 +1,12 @@
+#ifndef WIELOMIANY_TEST_POLY_CORE_H_H
+#define WIELOMIANY_TEST_POLY_CORE_H_H
+
+void TestPolynomialBuilding(void);
+void TestPolyAdd(void);
+void TestPolyMultiply(void);
+void TestEqualAndNeg(void);
+void TestDegree(void);
+void PolyTestEvaluation(void);
+void TestCore(void);
+
+#endif //WIELOMIANY_TEST_POLY_CORE_H_H
