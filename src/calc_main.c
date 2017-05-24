b3732a78df56dfa8c9f811afccaa2a6898bc548d
Wojciech Matusiak <wm382710@students.mimuw.edu.pl>
2017-05-24 13:57:29 +0200

    Dodana możliwość czytania z stdin

--- src/calc_main.c
+++ src/calc_main.c
@@ -8,6 +8,10 @@
 #define EXITCODE_FILE_OPEN_ERROR 2
 #define EXITCODE_FILE_SYNTAX_ERROR 3
 
+///Kiedy ta flaga jest zdefiniowana, program można po kompilacji wywołać z parametrem '-', aby czytał dane z stdin
+///W przeciwnym wypadku, zawsze oczekiwana jest nazwa pliku.
+#define ALLOW_STDIN_ARG
+
 
 int main(int argc, const char **argv)
 {
@@ -16,7 +20,15 @@ int main(int argc, const char **argv)
         return EXITCODE_INVALID_INVOCATION;
     }
 
-    FILE *source = fopen(argv[1], "r");
+    FILE *source;
+#ifdef ALLOW_STDIN_ARG
+    if (strcmp(argv[1], "-") != 0)
+        source = fopen(argv[1], "r");
+    else
+        source = stdin;
+#else
+    source = fopen(argv[1], "r");
+#endif
     if (source == NULL) {
         fprintf(stderr, "Could not open file '%s': %s", argv[1], strerror(errno));
         return EXITCODE_FILE_OPEN_ERROR;
